03f1082e27c7bccf1ecaba533011ad42e8bd6f4a
SagarNeupane <112553588+sagar-neupane0@users.noreply.github.com>
2024-05-15 18:08:19 +0545

    Update Portfolio.jsx
    added gaming setup

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../img.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "gaming setup;
 
 /**
  * Project list
