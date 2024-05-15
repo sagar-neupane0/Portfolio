3fa36c4d792285764bbf6f80ae4a282023a00faa
SagarNeupane <112553588+sagar-neupane0@users.noreply.github.com>
2024-05-15 18:30:33 +0545

    Update Portfolio.jsx

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../img.jpg";
+import image from "../images/img.jpg";
 
 const imageAltText = "gaming setup;
 
